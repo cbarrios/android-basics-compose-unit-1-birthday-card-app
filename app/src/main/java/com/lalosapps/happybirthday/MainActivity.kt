fa0d5d887a846770120f67d90b3eeb8e85b4a600
cbarrios <lalobarrios16@yahoo.com>
2022-05-16 12:26:06 -0400

    master: (4) Add an Image composable

--- app/src/main/java/com/lalosapps/happybirthday/MainActivity.kt
+++ app/src/main/java/com/lalosapps/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -11,6 +12,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.lalosapps.happybirthday.ui.theme.HappyBirthdayTheme
@@ -32,7 +34,7 @@ fun BirthdayCard() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            BirthdayGreetingWithText(
+            BirthdayGreetingWithImage(
                 message = "Happy Birthday Sam!",
                 from = "- from Emma"
             )
@@ -52,6 +54,15 @@ fun BirthdayGreetingWithText(message: String, from: String) {
             fontSize = 24.sp
         )
     }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
 }
 
 @Preview(name = "Light")
