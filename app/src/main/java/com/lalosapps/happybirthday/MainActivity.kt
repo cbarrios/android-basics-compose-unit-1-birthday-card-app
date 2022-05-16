d0da7b4a342efc31784d2c3486ae7831b4e772e3
cbarrios <lalobarrios16@yahoo.com>
2022-05-16 10:58:40 -0400

    master: clean template + "Build a simple app with text composables" Codelab

---
+++ app/src/main/java/com/lalosapps/happybirthday/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.lalosapps.happybirthday
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.lalosapps.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayCard()
+        }
+    }
+}
+
+@Composable
+fun BirthdayCard() {
+    HappyBirthdayTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            BirthdayGreetingWithText(
+                message = "Happy Birthday Sam!",
+                from = "- from Emma"
+            )
+        }
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun BirthdayCardPreview() {
+    BirthdayCard()
+}
