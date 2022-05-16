c1569d40a434338bca420931ed12b48b6516c073
cbarrios <lalobarrios16@yahoo.com>
2022-05-16 12:54:42 -0400

    master: (6) Position and scale the Image composable

--- app/src/main/java/com/lalosapps/happybirthday/MainActivity.kt
+++ app/src/main/java/com/lalosapps/happybirthday/MainActivity.kt
@@ -5,14 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -63,7 +62,11 @@ fun BirthdayGreetingWithImage(message: String, from: String) {
     Box {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
         )
         BirthdayGreetingWithText(message = message, from = from)
     }
